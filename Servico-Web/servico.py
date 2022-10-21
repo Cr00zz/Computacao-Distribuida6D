0ba54c015614f94938bd202f19233e9cb8591945
Gustavo E. Cruz <64294820+Cr00zz@users.noreply.github.com>
2022-10-21 08:11:50 -0300

    Create servico.py

---
+++ Servico-Web/servico.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+dolar = 5.22
+euro = 5.09
+
+app = Flask(__name__)
+
+array_conversao = []
+
+@app.route('http://nome_da_maquina.dominio/convertemoeda',methods=['GET'])
+def getTodasConversao():
+    return jsonify({'conversoes: \n':array_conversao})
+
+@app.route('http://nome_da_maquina.dominio/convertemoeda',methods=['POST'])
+def criaJsonMoeda():
+  real_dolar = request.json['real']*dolar
+  real_euro = request.json['real']*euro
+    data = {
+        'conversao': [
+        'real': request.json['real'],
+        'dolar': real_dolar,
+        'euro': real_euro,
+        ]
+    }
+    array_conversao.append(data)
+    return jsonify(data)
+if __name__ == '__main__':
+  app.run()
